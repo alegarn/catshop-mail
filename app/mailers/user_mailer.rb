bb2245fb43b5dcdb7ba88e6fa7bb6a0578e6707a
alegarn <alexgarnier78310@protonmail.com>
2022-03-17 11:51:29 +0100

    changes

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,10 @@
+class UserMailer < ApplicationMailer
+  default from: 'reygner0224@gmail.com'
+
+
+  def welcome_email(user)
+    @user = user
+    @url = 'https://catshop-io.herokuapp.com/users/sign_in'
+    mail(to: @user.email, subject: 'Bienvenue sur CatShop')
+  end
+end
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['API_KEY_SENDGRID'],
+  :password => ENV['SECRET_KEY_SENDGRID'],
+  :domain => 'https://catshop-mailer.herokuapp.com/',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
